2be63e4cbffeb161bfa6e5d6a6318355694012e0
RayanSheikh01 <=>
2022-01-19 04:50:04 +0000

    Finish program

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import sys
+import random
+import pandas as pd
+
+names = pd.read_csv('babynames-clean.csv')
+sex = input("What is the sex of the name you would like generated: (boy or girl) ")
+index = random.randint(1, len(names))
+while names.loc[index, 'sex'] != sex:
+    index = random.randint(1, len(names))
+
+name = names.loc[index, 'name']
+print(name)
